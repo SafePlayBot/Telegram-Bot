89035a71cdac1bf693f53bee1d6a9c25b32f9d6e
SafePlayBot <code@kreativrat.de>
2024-10-12 05:10:32 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 TOKEN = os.environ.get('BOT_TOKEN')
 
 # GIF URL
-GIF_URL = "https://i.ibb.co/42FJqZB/kripty.jpg"
+GIF_URL = "https://i.ibb.co/s6hjSVf/Df.gif"
 
 # Messages and offers
 WELCOME_MESSAGE = "🎰Welcome to SafePlay, your guide to the world of safe Crypto Casinos"
